42f4da2b4727b914a0ba064296fa1c0d193d903c
Matt Ronchetto <hello@doamatto.xyz>
2021-10-19 04:29:38 +0000

    fix: URL for asset

--- static/js/lazy-font.js
+++ static/js/lazy-font.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", () => {
      if (!document.fonts.check("12px EB Garamond")) {
-        let font = new FontFace("JetBrains Mono", "url(https://raw.githubusercontent.com/octaviopardo/EBGaramond12/master/fonts/webfonts/JetBrainsMono-Regular.woff2)", {
+        let font = new FontFace("JetBrains Mono", "url(https://raw.githubusercontent.com/octaviopardo/EBGaramond12/master/fonts/webfonts/EBGaramond-Regular.woff2)", {
             style: 'normal',
             unicodeRange: 'U+000-5FF',
             weight: '400'
